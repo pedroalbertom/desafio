930fc414482d6c9ae0141e0a49c7f75cb4d8deb3
Pedro Alberto <pedroalbertompontes@gmail.com>
2025-04-05 15:10:38 -0300

    Creating the angular project and the first component

--- dashboard-admin/src/app/app.component.ts
+++ dashboard-admin/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dashboard-admin';
