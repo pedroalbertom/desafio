296427e1ff813f1ae96adf0cca7c6739e5246e78
Pedro Alberto <pedroalbertompontes@gmail.com>
2025-04-06 18:12:40 -0300

    Added a button to add courses

---
+++ dashboard-admin/src/app/modals/register-course-dialog/register-course-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-course-dialog',
+  imports: [],
+  templateUrl: './register-course-dialog.component.html',
+  styleUrl: './register-course-dialog.component.css'
+})
+export class RegisterCourseDialogComponent {
+
+}
