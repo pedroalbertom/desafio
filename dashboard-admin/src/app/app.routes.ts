e78bafb2608227932e27a0ec04771f66149d331b
Pedro Alberto <pedroalbertompontes@gmail.com>
2025-04-06 12:55:19 -0300

    Created the logout component

--- dashboard-admin/src/app/app.routes.ts
+++ dashboard-admin/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LogoutComponent } from './logout/logout.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'logout', component: LogoutComponent},
 ];
 
 @NgModule({
